c13de1a4f718d314cfd409b31651ff8a494fd4df
silvacook <robertsilva@yahoo.com>
2025-02-24 16:53:25 -0800

    Fix: Resolved isAdmin import error in categories route

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,40 +1,57 @@
+// src/app/utils/auth.js (create this file if it doesn't exist)
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import {UserInfo} from "@/models/UserInfo";
+
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({ email: userEmail });
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
+
+// src/app/api/categories/route.js
 import mongoose from "mongoose";
 import { Category } from "../../../models/Category";
-import { isAdmin } from "../auth/[...nextauth]/route";
+import { isAdmin } from "../../utils/auth"; // Import from the helper file
 
 export async function POST(req) {
-    mongoose.connect(process.env.MONGO_URL);
-    const {name} = await req.json();
-    if (await isAdmin()) {
-        const categoryDoc = await Category.create({name});
-    } else {
-        return Response.json({});
-    }
-    return Response.json(categoryDoc);
+  mongoose.connect(process.env.MONGO_URL);
+  const { name } = await req.json();
+  if (await isAdmin()) {
+    const categoryDoc = await Category.create({ name });
+    return Response.json(categoryDoc); // Return the created document
+  } else {
+    return Response.json({}); // Return an empty object if not admin
+  }
 }
 
 export async function PUT(req) {
-    mongoose.connect(process.env.MONGO_URL);
-    const { _id, name } = await req.json();
-    if (await isAdmin()) {
-        await Category.updateOne({_id}, {name});
-    }
-    return Response.json(true);
+  mongoose.connect(process.env.MONGO_URL);
+  const { _id, name } = await req.json();
+  if (await isAdmin()) {
+    await Category.updateOne({ _id }, { name });
+  }
+  return Response.json(true);
 }
 
 export async function GET() {
-    mongoose.connect(process.env.MONGO_URL);
-    return Response.json(
-        await Category.find()
-    )
+  mongoose.connect(process.env.MONGO_URL);
+  return Response.json(await Category.find());
 }
 
 export async function DELETE(req) {
-    mongoose.connect(process.env.MONGO_URL);
-    const url = new URL(req.url);
-    const _id = url.searchParams.get("_id");
-    if(await isAdmin()) {
-        await Category.deleteOne({_id});
-    }
-    return Response.json(true);
+  mongoose.connect(process.env.MONGO_URL);
+  const url = new URL(req.url);
+  const _id = url.searchParams.get("_id");
+  if (await isAdmin()) {
+    await Category.deleteOne({ _id });
+  }
+  return Response.json(true);
 }

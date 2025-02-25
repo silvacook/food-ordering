edd25cc132c61d2e20987d46cd43740fd0ea2adb
silvacook <robertsilva@yahoo.com>
2025-02-24 16:33:20 -0800

    Resolved the error

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -7,71 +7,68 @@ import toast from "react-hot-toast";
 export const CartContext = createContext({});
 
 export function cartProductPrice(cartProduct) {
-  let price = cartProduct.basePrice;
-  if (cartProduct.size) {
-    price += cartProduct.size.price;
-  }
-  if (cartProduct.extras?.length > 0) {
-    for (const extra of cartProduct.extras) {
-      price += extra.price;
+    let price = cartProduct.basePrice;
+    if (cartProduct.size) {
+        price += cartProduct.size.price;
     }
-  }
-  return price;
+    if(cartProduct.extras?.length > 0) {
+        for(const extra of cartProduct.extras) {
+            price += extra.price;
+        }
+    }
+    return price;
 }
 
-export function AppProvider({ children }) {
-  const [cartProducts, setCartProducts] = useState();
-  const ls = typeof window!== "undefined"? window.localStorage: null;
+export function AppProvider({children}) {
+    const [cartProducts, setCartProducts] = useState([]);
+    const ls = typeof window !== "undefined" ? window.localStorage : null;
 
-  useEffect(() => {
-    if (ls && ls.getItem("cart")) {
-      setCartProducts(JSON.parse(ls.getItem("cart")));
+    useEffect(() => {
+        if(ls && ls.getItem("cart")) {
+            setCartProducts( JSON.parse(ls.getItem("cart")))
+        }
+    }, []);
+
+    function clearCart() {
+        setCartProducts([]);
+        saveCartProductsToLocalStorage([]);
     }
-  }, [ls]);
 
-  function clearCart() {
-    setCartProducts();
-    saveCartProductsToLocalStorage();
-  }
-
-  function removeCartProduct(indexToRemove) {
-    setCartProducts((prevCartProducts) => {
-      const newConstProducts = prevCartProducts.filter(
-        (v, index) => index!== indexToRemove
-      );
-      saveCartProductsToLocalStorage(newConstProducts);
-      return newConstProducts;
-    });
-    toast.success("Product removed");
-  }
-
-  function saveCartProductsToLocalStorage(cartProducts) {
-    if (ls) {
-      ls.setItem("cart", JSON.stringify(cartProducts));
+    function removeCartProduct(indexToRemove) {
+        setCartProducts(prevCartProducts => {
+            const newConstProducts = prevCartProducts
+                .filter((v, index) => index !== indexToRemove);
+            saveCartProductsToLocalStorage(newConstProducts);
+            return newConstProducts; 
+        });
+        toast.success("Product removed");
     }
-  }
 
-  function addToCart(product, size = null, extras =) {
-    setCartProducts((prevProducts) => {
-      const cartProduct = {...product, size, extras };
-      const newProducts = [...prevProducts, cartProduct];
-      saveCartProductsToLocalStorage(newProducts);
-      return newProducts;
-    });
-  }
-  return (
-    <SessionProvider>
-      <CartContext.Provider
-        value={{
-          cartProducts,
-          setCartProducts,
-          addToCart,
-          removeCartProduct,
-          clearCart,
-        }}
-      >
-        {children}
-      </CartContext.Provider>
-    </SessionProvider>
-  );
+    function saveCartProductsToLocalStorage(cartProducts) {
+        if(ls) {
+            ls.setItem('cart', JSON.stringify(cartProducts));
+        }
+    }
+
+    function addToCart(product, size=null, extras=[]) {
+        setCartProducts(prevProducts => {
+            const cartProduct = {...product, size, extras};
+            const newProducts = [... prevProducts, cartProduct];
+            saveCartProductsToLocalStorage(newProducts);
+            return newProducts;
+        });
+    }
+    return (
+        <SessionProvider>
+            <CartContext.Provider value={{
+                cartProducts,
+                setCartProducts,
+                addToCart,
+                removeCartProduct,
+                clearCart,
+            }}>
+            {children}
+            </CartContext.Provider>
+        </SessionProvider>
+    )
 }

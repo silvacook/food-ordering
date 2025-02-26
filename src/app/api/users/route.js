4f19503045d526516de14c113a814a9fc1d15b9f
silvacook <robertsilva@yahoo.com>
2025-02-25 17:02:02 -0800

    Fix: little errors

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,13 +1,14 @@
-import {isAdmin} from "@/app/api/auth/[...nextauth]/route";
-import {User} from "@/models/User";
+import { isAdmin } from "@/utils/auth";
+import { User } from "@/models/User";
 import mongoose from "mongoose";
 
-export async function GET() {
+export async function GET(req) {
   mongoose.connect(process.env.MONGO_URL);
-  if (await isAdmin()) {
+
+  if (await isAdmin(req)) {
     const users = await User.find();
     return Response.json(users);
   } else {
-    return Response.json([]);
+    return Response.json({ error: "Unauthorized" }, { status: 403 });
   }
 }

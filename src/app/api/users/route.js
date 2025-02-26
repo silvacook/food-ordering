a28dac05427494f3ce34ff6c80b9bc04769554d4
silvacook <robertsilva@yahoo.com>
2025-02-25 17:15:50 -0800

    Fix: little errors 6

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,14 +1,15 @@
 import { isAdmin } from "@/utils/auth";
 import { User } from "@/models/User";
 import mongoose from "mongoose";
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
 
   if (await isAdmin(req)) {
     const users = await User.find();
-    return Response.json(users);
+    return NextResponse.json(users);
   } else {
-    return Response.json({ error: "Unauthorized" }, { status: 403 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
   }
 }

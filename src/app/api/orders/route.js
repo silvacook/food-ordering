8206d1ea0c87fdb651a876d70792432af8e6e26b
silvacook <robertsilva@yahoo.com>
2025-02-25 18:46:28 -0800

    Fix error 14

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,5 +1,4 @@
-import { authOptions } from "@/libs/authOptions";
-import { isAdmin } from "@/utils/auth";
+import {authOptions, isAdmin} from "@/app/api/auth/[...nextauth]/route";
 import {Order} from "@/models/Order";
 import mongoose from "mongoose";
 import {getServerSession} from "next-auth";

55da5bd85b1bf3d543de6499f63a54918577d1ea
silvacook <robertsilva@yahoo.com>
2025-02-25 18:23:50 -0800

    Fix error 8

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -1,93 +1,77 @@
-"use client";
-
-import { CartContext, cartProductPrice } from "@/components/AppContext";
-import SectionHeaders from "@/components/layout/SectionHeaders";
-import { useParams } from "next/navigation";
+'use client';
+import {CartContext, cartProductPrice} from "@/components/AppContext";
 import AddressInputs from "@/components/layout/AddressInputs";
-import { useContext, useEffect, useState } from "react";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 import CartProduct from "@/components/menu/CartProduct";
+import {useParams} from "next/navigation";
+import {useContext, useEffect, useState} from "react";
 
 export default function OrderPage() {
-  const { clearCart } = useContext(CartContext);
+  const {clearCart} = useContext(CartContext);
   const [order, setOrder] = useState();
   const [loadingOrder, setLoadingOrder] = useState(true);
-  const { id } = useParams();
-
+  const {id} = useParams();
   useEffect(() => {
-    if (typeof window!== "undefined") {
-      if (window.location.href.includes("clear-cart=1")) {
+    if (typeof window.console !== "undefined") {
+      if (window.location.href.includes('clear-cart=1')) {
         clearCart();
       }
     }
     if (id) {
       setLoadingOrder(true);
-      fetch("/api/orders?_id=" + id)
-      .then((res) => {
-          res.json().then((orderData) => {
-            setOrder(orderData);
-            setLoadingOrder(false);
-          });
+      fetch('/api/orders?_id='+id).then(res => {
+        res.json().then(orderData => {
+          setOrder(orderData);
+          setLoadingOrder(false);
         });
+      })
     }
-  }, [id, clearCart]);
+  }, []);
 
   let subtotal = 0;
   if (order?.cartProducts) {
-    for (const product of order.cartProducts) {
+    for (const product of order?.cartProducts) {
       subtotal += cartProductPrice(product);
     }
   }
-
-  const deliveryFee = 5;
-  const total = subtotal + deliveryFee;
 
   return (
     <section className="max-w-2xl mx-auto mt-8">
       <div className="text-center">
         <SectionHeaders mainHeader="Your order" />
         <div className="mt-4 mb-8">
-          <p>Thanks for your order</p>
+          <p>Thanks for your order.</p>
           <p>We will call you when your order will be on the way.</p>
         </div>
       </div>
-      {loadingOrder && <div>Loading order...</div>}
+      {loadingOrder && (
+        <div>Loading order...</div>
+      )}
       {order && (
         <div className="grid md:grid-cols-2 md:gap-16">
           <div>
-            {order.cartProducts.map((product, index) => (
-              <CartProduct
-                key={index}
-                product={product}
-                index={index}
-                onRemove={() => {}}
-              />
+            {order.cartProducts.map(product => (
+              <CartProduct key={product._id} product={product} />
             ))}
             <div className="text-right py-2 text-gray-500">
-              Subtotal:{" "}
-              <span className="text-black font-bold inline-block w-8">
-                ${subtotal}
-              </span>
+              Subtotal:
+              <span className="text-black font-bold inline-block w-8">${subtotal}</span>
               <br />
-              Delivery:{" "}
-              <span className="text-black font-bold inline-block w-8">
-                ${deliveryFee}
-              </span>
+              Delivery:
+              <span className="text-black font-bold inline-block w-8">$5</span>
               <br />
-              Total:{" "}
+              Total:
               <span className="text-black font-bold inline-block w-8">
-                ${total}
+                ${subtotal + 5}
               </span>
             </div>
           </div>
           <div>
             <div className="bg-gray-100 p-4 rounded-lg">
-              {order.streetAddress && (
-                <AddressInputs
-                  disabled={true}
-                  addressProps={order}
-                  setAddressProp={() => {}}
-                />
-              )}
+              <AddressInputs
+                disabled={true}
+                addressProps={order}
+              />
             </div>
           </div>
         </div>

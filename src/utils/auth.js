fbe89a15e6e16299c2dc3156fc5b6590fd326f6d
silvacook <robertsilva@yahoo.com>
2025-03-12 13:05:47 -0700

    Fix: details and auth 9

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,50 +1,13 @@
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import clientPromise from "@/libs/mongoConnect";
-import { User } from "@/models/User";
-import { UserInfo } from "@/models/UserInfo";
-import bcrypt from "bcrypt";
-import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
+import { User } from "@/models/User";
+import mongoose from "mongoose";
 
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({ email: credentials?.email });
-        const passwordOk = user && bcrypt.compareSync(credentials?.password, user.password);
-
-        return passwordOk ? user : null;
-      },
-    }),
-  ],
-};
-
-export async function isAdmin() {
+export async function isAdmin(req) {
   await mongoose.connect(process.env.MONGO_URL);
   
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(req);
   if (!session?.user?.email) return false;
 
-  const userInfo = await UserInfo.findOne({ email: session.user.email });
-  if (!userInfo) return false;
-  
-  return userInfo.admin === true;
+  const user = await User.findOne({ email: session.user.email });
+  return user?.role === "admin";
 }

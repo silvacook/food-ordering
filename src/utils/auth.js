c8af5d6d6a0d040683bed2253de2333fd0668d76
silvacook <robertsilva@yahoo.com>
2025-03-12 13:58:14 -0700

    Fix: details again 1

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,14 +1,11 @@
-// src/libs/isAdmin.js
-
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // Import authOptions
 import { User } from "@/models/User";
 import mongoose from "mongoose";
 
 export async function isAdmin(req) {
   await mongoose.connect(process.env.MONGO_URL);
-
-  const session = await getServerSession(req, authOptions); // Pass authOptions here
+  
+  const session = await getServerSession(req);
   if (!session?.user?.email) return false;
 
   const user = await User.findOne({ email: session.user.email });

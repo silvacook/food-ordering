b55ce6853c31dd77a82377fa62644ce43fa330a5
silvacook <robertsilva@yahoo.com>
2025-03-12 12:43:27 -0700

    Fix: details and auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -9,11 +9,8 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
-  session: {
-    strategy: "jwt", // ✅ Add this line to enforce JWT session handling
-  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,

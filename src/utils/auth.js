9387469645c74b97e820985cf2c92bb04fd141e5
silvacook <robertsilva@yahoo.com>
2025-03-12 12:56:47 -0700

    Fix: details and auth 7

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,11 +1,12 @@
 import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/auth"; // Import authOptions from your auth file
 import { User } from "@/models/User";
 import mongoose from "mongoose";
 
-export async function isAdmin(req) {
+export async function isAdmin() {
   await mongoose.connect(process.env.MONGO_URL);
   
-  const session = await getServerSession(req);
+  const session = await getServerSession(authOptions);
   if (!session?.user?.email) return false;
 
   const user = await User.findOne({ email: session.user.email });
